5e6b72d41ac57c5d010166411a718f4e410bab91
Innocent Josiah Patrick <innocentjosiah57@gmail.com>
2023-11-28 15:58:37 +0100

    survey controller

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -5,7 +5,14 @@ namespace App\Http\Controllers;
 use App\Http\Requests\SurveyStoreeRequest;
 use App\Http\Resources\SurveyResource;
 use App\Models\Survey;
+use App\Models\SurveyQuestion;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
+use Illuminate\Validation\Rules\Enum;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
 
 class SurveyController extends Controller
 {
@@ -30,22 +37,60 @@ class SurveyController extends Controller
     public function store(SurveyStoreeRequest $request)
     {
         $data = $request->validated();
+        // check if data has image
+        if (isset($data['image'])) {
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+        }
+
+        $survey = Survey::create($data);
+
+        // create a new question
+        foreach ($data['questions'] as $question) {
+            $question['survey_id'] = $survey->id;
+            $this->createQuestion($question);
+
+            return new SurveyResource($survey);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Survey $survey, Request $request)
     {
-        //
+        $user = $request->user();
+        if ($user->id !== $survey->user_id) {
+            return abort(403, 'Unauthorized action');
+        }
+
+        return new SurveyResource($survey);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Survey $survey)
     {
-        //
+        // validate the request
+        $data = $request->validated();
+
+        // check if image exist
+
+        if (isset($data['image'])) {
+            $relativePath = $this->saveImage($data['image']);
+            $data['image'] = $relativePath;
+        }
+
+        // delete old image
+        if ($survey->image) {
+            $absolutePath = public_path($survey->image);
+            File::delete($absolutePath);
+        }
+        $survey->update($data);
+
+        // update the questions in the database
+        $existingIds = $survey->questions()->pluck('id')->toArray();
     }
 
     /**
@@ -54,5 +99,75 @@ class SurveyController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    /**
+     * Save image in local file system and return saved image path
+     *
+     * @param $image
+     * @throws \Exception
+     * 
+     */
+    private function saveImage($image)
+    {
+        // Check if image is valid base64 string
+        if (preg_match('/^data:image\/(\w+);base64,/', $image, $type)) {
+            // Take out the base64 encoded text without mime type
+            $image = substr($image, strpos($image, ',') + 1);
+            // Get file extension
+            $type = strtolower($type[1]); // jpg, png, gif
+
+            // Check if file is an image
+            if (!in_array($type, ['jpg', 'jpeg', 'gif', 'png'])) {
+                throw new \Exception('invalid image type');
+            }
+            $image = str_replace(' ', '+', $image);
+            $image = base64_decode($image);
+
+            if ($image === false) {
+                throw new \Exception('base64_decode failed');
+            }
+        } else {
+            throw new \Exception('did not match data URI with image data');
+        }
+
+        $dir = 'images/';
+        $file = Str::random() . '.' . $type;
+        $absolutePath = public_path($dir);
+        $relativePath = $dir . $file;
+        if (!File::exists($absolutePath)) {
+            File::makeDirectory($absolutePath, 0755, true);
+        }
+        file_put_contents($relativePath, $image);
+
+        return $relativePath;
+    }
+
+    /**
+     * Create a question and return
+     *
+     * @param $data
+     * @return mixed
+     * @throws \Illuminate\Validation\ValidationException
+     * 
+     */
+
+    private function createQuestion($data)
+    {
+        if (is_array($data['data'])) {
+            $data['data'] = json_encode($data['data']);
+        }
+        $validator = FacadesValidator::make(
+            $data,
+            [
+                'question' => 'required|string',
+                'type' => ['required', new Enum(QuestionTypeEnum::class)],
+                'descripton' => 'nullable |string',
+                'data' => 'present',
+                'survey_id' => 'exists:App\Models\Survey\,id'
+
+            ]
+        );
+        return  SurveyQuestion::creae($validator->validated());
     }
 }
